d164045f947264111fb146d044e3214302c68b95
iPlanC <planc2333@outlook.com>
2020-08-11 21:03:18 +0800

    init

---
+++ 8.3/hello.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-08-11 17:51:33
+ * @LastEditTime: 2020-08-11 18:52:59
+ * @FilePath: /Practical-Training-Neusoft/8.3/hello.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
