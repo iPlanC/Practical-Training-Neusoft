5221b758eb2e9dc7def946eead78864f110ed182
iPlanC <planc2333@outlook.com>
2020-08-12 12:22:57 +0800

    renamed 8.12 2.c Author to "邴哲松老师"

--- 8.12/2.c
+++ 8.12/2.c
@@ -1,48 +1,49 @@
 /*
- * @Author: PlanC
+ * @Author: 邴哲松老师
  * @Date: 2020-08-12 09:28:33
- * @LastEditTime: 2020-08-12 10:56:42
- * @FilePath: /Practical-Training-Neusoft/8.12/2.c
+ * @LastEditTime: 2020-08-12 12:21:17
+ * @FilePath: \Practical-Training-Neusoft\8.12\2.c
  */
 #include <stdio.h>
 
-#define MAXTITLE	81
-#define MAXAUTH		81
-#define MAXBKS		100
+#define MAXTITLE 81
+#define MAXAUTH 81
+#define MAXBKS 100
 
 struct book {
-	char title[MAXTITLE];
-	char author[MAXAUTH];
-	float price;
+    char title[MAXTITLE];
+    char author[MAXAUTH];
+    float price;
 };
 
 int main(void) {
-	struct book library[MAXBKS];
-	int count = 0;
-	int i;
+    struct book library[MAXBKS];
+    int count = 0;
+    int i;
 
-	printf("Please enter the book title:\n");
+    printf("Please enter the book title:\n");
 
-	while(count < MAXBKS && gets(library[count].title) && library[count].title[0] != '\0') {
-		printf("Now enter the author:\n");
-		gets(library[count].author);
-		printf("Now enter the price:\n");
-		scanf("%f", &library[count].price);
-		getchar();
-		count++;
-		if(count < MAXBKS) {
-			printf("Please enter the book title:\n");
+    while (count < MAXBKS && gets(library[count].title) &&
+           library[count].title[0] != '\0') {
+        printf("Now enter the author:\n");
+        gets(library[count].author);
+        printf("Now enter the price:\n");
+        scanf("%f", &library[count].price);
+        getchar();
+        count++;
+        if (count < MAXBKS) {
+            printf("Please enter the book title:\n");
         }
-	}
-
-	if(count > 0) {
-		printf("List of books:\n");
-        printf("title\t\tauthor\t\tprice\n");
-		for(i = 0; i < count; i++)
-			printf("<<%s>>\tby %s:\t%.2f\n", library[i].title, library[i].author, library[i].price);
-	}
-	else {
-		printf("No books.\n");
     }
-	return 0;
+
+    if (count > 0) {
+        printf("List of books:\n");
+        printf("title\t\tauthor\t\tprice\n");
+        for (i = 0; i < count; i++)
+            printf("<<%s>>\tby %s:\t%.2f\n", library[i].title,
+                   library[i].author, library[i].price);
+    } else {
+        printf("No books.\n");
+    }
+    return 0;
 }
