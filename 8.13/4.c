77929cc25a93def4276fcb8776974b4980eb0081
iPlanC <planc2333@outlook.com>
2020-08-13 10:19:38 +0800

    8.13 4.c "struct name chars"

---
+++ 8.13/4.c
@@ -0,0 +1,36 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-08-13 09:40:19
+ * @LastEditTime: 2020-08-13 10:19:00
+ * @FilePath: /Practical-Training-Neusoft/8.13/4.c
+ */
+#include <stdio.h>
+#include <string.h>
+#define SIZE 30
+
+struct NAME {
+    char first[SIZE];
+    char last[SIZE];
+    int size;
+};
+
+void showinfo(struct NAME name) {
+    printf("%s %s, your name has %d char\n", name.first, name.last, name.size);
+}
+
+void makeinfo(struct NAME name) {
+    name.size = strlen(name.first) + strlen(name.last);
+    showinfo(name);
+}
+
+void getinfo() {
+    struct NAME name;
+    printf("input your name\n");
+    scanf("%s %s", name.first, name.last);
+    name.size = 0;
+    makeinfo(name);
+}
+
+int main() {
+    getinfo();
+}
