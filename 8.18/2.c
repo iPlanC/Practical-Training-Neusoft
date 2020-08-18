8f30a7b7f89b1414d64aad44ecf027327139f737
iPlanC <planc2333@outlook.com>
2020-08-18 10:26:12 +0800

    update 8.18 2.c

--- 8.18/2.c
+++ 8.18/2.c
@@ -1,8 +1,8 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-18 09:48:08
- * @LastEditTime: 2020-08-18 10:20:38
- * @FilePath: /Practical-Training-Neusoft/8.18/2.c
+ * @LastEditTime: 2020-08-18 10:25:56
+ * @FilePath: \Practical-Training-Neusoft\8.18\2.c
  */
 
 #include <stdio.h>
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 		printf("usage: %s <filename> <filename>\n", argv[0]);
 		exit(1);
 	}
-	
+
 	FILE *file1 = NULL;
 	FILE *file2 = NULL;
 	if ((file1 = fopen(argv[1], "r")) == NULL) {
@@ -29,13 +29,12 @@ int main(int argc, char *argv[]) {
 
 	char str1[SIZE] = {'\0'};
 	char str2[SIZE] = {'\0'};
-	while (1) {
+	while (feof(file1) && feof(file2)) {
 		if (fgets(str1, SIZE, file1) != NULL) fputs(str1, stdout);
 		if (fgets(str2, SIZE, file2) != NULL) fputs(str2, stdout);
-		if (feof(file1) && feof(file2)) break;
 	}
 	fclose(file1);
 	fclose(file2);
-	
+
 	return 0;
 }
