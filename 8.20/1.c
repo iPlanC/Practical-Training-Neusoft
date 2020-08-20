7864892ce14c19d55216afb0330845691c298f6f
iPlanC <planc2333@outlook.com>
2020-08-20 11:32:10 +0800

    update 8.20 2.c

--- 8.20/1.c
+++ 8.20/1.c
@@ -1,7 +1,7 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-20 08:39:04
- * @LastEditTime: 2020-08-20 10:49:21
+ * @LastEditTime: 2020-08-20 11:11:18
  * @FilePath: /Practical-Training-Neusoft/8.20/1.c
  */
 
@@ -9,19 +9,23 @@
 #include <string.h>
 
 int getvalue(int value, int offset) {
-    return (value >> (offset - 1)) & 0x0001;
+	return (value >> (offset - 1)) & 0x0001;
 }
 
 int stoi(char *number) {
-    int i = 0;
-    int result = 0;
-    for (i = 0; i < strlen(number); i++) {
-        result = result * 10 + (number[i] - '0');
-    }
-    return result;
+	int i = 0;
+	int result = 0;
+	for (i = 0; i < strlen(number); i++) {
+		result = result * 10 + (number[i] - '0');
+	}
+	return result;
 }
 
 int main(int argc, char *argv[]) {
-    printf("number: %d, %dth offset: %d\n", stoi(argv[1]), stoi(argv[2]), getvalue(stoi(argv[1]), stoi(argv[2])));
-    return 0;
+	if (argc != 3) {
+		printf("Usage: %s <integer> <integer>", argv[0]);
+		exit(1);
+	}
+	printf("number: %d, %dth offset: %d\n", stoi(argv[1]), stoi(argv[2]), getvalue(stoi(argv[1]), stoi(argv[2])));
+	return 0;
 }
---
+++ 8.20/2.c
@@ -0,0 +1,52 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-08-20 11:08:22
+ * @LastEditTime: 2020-08-20 11:31:24
+ * @FilePath: /Practical-Training-Neusoft/8.20/2.c
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SIZE 81
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		printf("Usage: %s <integer> <filename>", argv[0]);
+		exit(1);
+	}
+	
+	FILE *fp = NULL;
+	if ((fp = fopen(argv[2], "w")) == NULL) {
+		printf("can't open file \"%s\"", argv[2]);
+	}
+	rewind(fp);
+	fputs(argv[1], fp); fclose(fp);
+
+	int i = 0;
+	int j = 0;
+	int bucket[10] = {0};
+	char array[SIZE] = {'\0'};
+	char result[SIZE] = {'\0'};
+	if ((fp = fopen(argv[2], "r")) == NULL) {
+		printf("can't open file \"%s\"", argv[2]);
+	}
+	while (fgets(array, SIZE, fp) != NULL) {
+		for (i = 0; i < strlen(array); i++) {
+			if (bucket[(int)(array[i] - '0')] == 0) {
+				bucket[(int)(array[i] - '0')]++;
+				// printf("%d", array[i] - '0');
+				result[j++] = array[i];
+			}
+		}
+	}
+
+	if ((fp = fopen(argv[2], "w")) == NULL) {
+		printf("can't open file \"%s\"", argv[2]);
+	}
+	rewind(fp);
+	fputs(result, fp); fclose(fp);
+	printf("%s\n", result);
+	return 0;
+}
