73effed14a72eca09c011ad27b009c2d3ab07a29
iPlanC <planc2333@outlook.com>
2020-08-20 16:42:14 +0800

    create 8.20 4.c

--- 8.20/4.c
+++ 8.20/4.c
@@ -1,7 +1,7 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-20 16:00:52
- * @LastEditTime: 2020-08-20 16:13:08
+ * @LastEditTime: 2020-08-20 16:40:24
  * @FilePath: /Practical-Training-Neusoft/8.20/4.c
  */
 
@@ -11,39 +11,39 @@
 #include <string.h>
 
 char *my_toupper(char *str) {
-    int i = 0;
-    for (i = 0; i < strlen(str); i++) {
-        str[i] = toupper(str[i]);
-    }
-    return str;
+	int i = 0;
+	for (i = 0; i < strlen(str); i++) {
+		str[i] = toupper(str[i]);
+	}
+	return str;
 }
 
 char *my_tolower(char *str) {
-    int i = 0;
-    for (i = 0; i < strlen(str); i++) {
-        str[i] = tolower(str[i]);
-    }
-    return str;
+	int i = 0;
+	for (i = 0; i < strlen(str); i++) {
+		str[i] = tolower(str[i]);
+	}
+	return str;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage %s <-pul> <string>", argv[0]);
-        exit(0);
-    }
+	if (argc != 3) {
+		printf("Usage %s <-pul> <string>", argv[0]);
+		exit(0);
+	}
 
-    switch (argv[1][1]) {
-        case 'p':
-            printf("%s\n", argv[2]);
-            break;
-        case 'u':
-            printf("%s\n", my_toupper(argv[2]));
-            break;
-        case 'l':
-            printf("%s\n", my_tolower(argv[2]));
-            break;
-        default:
-            break;
-    }
-    return 0;
+	switch (argv[1][1]) {
+		case 'p':
+			printf("%s\n", argv[2]);
+			break;
+		case 'u':
+			printf("%s\n", my_toupper(argv[2]));
+			break;
+		case 'l':
+			printf("%s\n", my_tolower(argv[2]));
+			break;
+		default:
+			break;
+	}
+	return 0;
 }
