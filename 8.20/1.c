547f480b290fee7a748166622a6205778ab02b23
iPlanC <planc2333@outlook.com>
2020-08-20 10:49:46 +0800

    update 8.20 1.c

--- 8.20/1.c
+++ 8.20/1.c
@@ -1,22 +1,27 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-20 08:39:04
- * @LastEditTime: 2020-08-20 10:20:20
+ * @LastEditTime: 2020-08-20 10:49:21
  * @FilePath: /Practical-Training-Neusoft/8.20/1.c
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int getvalue(int value, int offset) {
     return (value >> (offset - 1)) & 0x0001;
 }
 
-int main() {
-    int value = 0;
-    int offset = 0;
-    printf("input integer and offset\n");
-	while (scanf("%d %d", &value, &offset) == 2) {
-        printf("number: %d, %dth offset: %d\n", value, offset, getvalue(value, offset));
+int stoi(char *number) {
+    int i = 0;
+    int result = 0;
+    for (i = 0; i < strlen(number); i++) {
+        result = result * 10 + (number[i] - '0');
     }
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    printf("number: %d, %dth offset: %d\n", stoi(argv[1]), stoi(argv[2]), getvalue(stoi(argv[1]), stoi(argv[2])));
     return 0;
 }
