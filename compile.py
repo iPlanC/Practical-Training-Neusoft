8a319f328d566d760012aa53f1ac036a3fe7349a
iPlanC <planc2333@outlook.com>
2020-08-21 17:59:59 +0800

    create compile.py

---
+++ compile.py
@@ -0,0 +1,45 @@
+'''
+Author: PlanC
+Date: 2020-08-21 16:18:23
+LastEditTime: 2020-08-21 17:54:30
+FilePath: \Practical-Training-Neusoft\compile.py
+'''
+
+import os
+import platform
+import sys
+import time
+
+def get_list(dir, filelist):
+    newdir = dir
+    if os.path.isfile(dir):
+        if dir.endswith('.c'):
+            filelist.append(os.path.splitext(dir)[0])
+        if dir.endswith('.exe') or dir.endswith('.out'):
+            os.remove(dir)
+            print('\rdeleted ' + dir, end = '')
+
+    if os.path.isdir(dir):
+        for s in os.listdir(dir):
+            if s == '.git' or s == '.vscode':
+                continue
+            newdir = os.path.join(dir, s)
+            get_list(newdir, filelist)
+    return filelist
+
+
+if __name__ == '__main__':
+    count = 0
+    list = get_list('.', [])
+    os.system('clear || cls')
+    print(str(len(list)) + ' files')
+    for e in list:
+        count = count + 1
+        print('\rprogress: ' + str(len(list)) + ' / ' + str(count), end = '')
+        if platform.system() == 'Windows':
+            print('\tgcc ' + e + '.c -o ' + e + '.exe')
+            os.system('gcc ' + e + '.c -o ' + e + '.exe')
+
+        if platform.system() == 'Linux':
+            print('\tgcc ' + e + '.c -o ' + e + '.out')
+            os.system('gcc ' + e + '.c -o ' + e + '.out -lm')
