7d717aea64e8c2fd7836e661c5a27599b9bca376
iPlanC <planc2333@outlook.com>
2020-08-21 19:48:49 +0800

    progressbar update

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 '''
 Author: PlanC
 Date: 2020-08-21 16:18:23
-LastEditTime: 2020-08-21 19:20:48
+LastEditTime: 2020-08-21 19:48:16
 FilePath: \Practical-Training-Neusoft\compile.py
 '''
 
@@ -9,6 +9,7 @@ import os
 import platform
 import sys
 import time
+from alive_progress import alive_bar
 
 def get_list(dir, filelist):
     newdir = dir
@@ -29,21 +30,21 @@ def get_list(dir, filelist):
 
 
 if __name__ == '__main__':
-    count = 0
     list = get_list('.', [])
     os.system('clear || cls')
     print(str(len(list)) + ' files')
-    for e in list:
-        count = count + 1
-        print('progress: ' + str(len(list)) + ' / ' + str(count), end = '')
-        if platform.system() == 'Windows':
-            if (len(e.split('\\')) == 3):
-                print('\tgcc\t' + e + '.c\t-o\t' + e + '.exe')
-                if os.system('gcc ' + e + '.c -o ' + e + '.exe') != 0:
-                    break
+    with alive_bar(len(list)) as bar:
+        for e in list:
+            bar()
+            time.sleep(0.1)
+            if platform.system() == 'Windows':
+                if (len(e.split('\\')) == 3):
+                    #print('\tgcc\t' + e + '.c\t-o\t' + e + '.exe', end = '')
+                    if os.system('gcc ' + e + '.c -o ' + e + '.exe') != 0:
+                        break
 
-        if platform.system() == 'Linux':
-            if (len(e.split('/')) == 3):
-                print('\tgcc\t' + e + '.c\t-o\t' + e + '.out\t-lm')
-                if os.system('gcc ' + e + '.c -o ' + e + '.out -lm') != 0:
-                    break
+            if platform.system() == 'Linux':
+                if (len(e.split('/')) == 3):
+                    #print('\tgcc\t' + e + '.c\t-o\t' + e + '.out\t-lm')
+                    if os.system('gcc ' + e + '.c -o ' + e + '.out -lm') != 0:
+                        break
